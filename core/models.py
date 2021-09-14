2915150375836d3edd1791b3dcf57bf3dc792154
Taha <taha@firstsource.tech>
2021-09-14 11:53:47 +0430

    feat: data models completed

--- core/models.py
+++ core/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from datetime import timedelta
 from rest_framework.exceptions import NotFound
+from . import const
 
 
 class BaseModel(models.Model):
@@ -55,11 +58,6 @@ class BaseUserModel(AbstractBaseUser, PermissionsMixin):
         return self.username
 
 
-class Customer(models.Model):
-    user = models.OneToOneField(BaseUserModel, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=255)
-
-
 class Clerk(models.Model):
     user = models.OneToOneField(BaseUserModel, on_delete=models.CASCADE)
 
@@ -73,3 +71,35 @@ class Branch(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(BaseUserModel, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=255)
+
+
+class Account(BaseModel):
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='accounts')
+    is_active = models.BooleanField(default=True)
+    balance = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+
+
+class Transaction(BaseModel):
+    from_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name='debits')
+    to_account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name='deposits')
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    type = models.CharField(max_length=255, choices=const.TRANSACTION_TYPE_CHOICES)
+
+
+class Loan(BaseModel):
+    interest_rate = models.DecimalField(max_digits=4, decimal_places=2, default=0.2)
+    annual_interest_rate = models.DecimalField(max_digits=4, decimal_places=2, default=0.1)
+    amount = models.PositiveBigIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000000)])
+    type = models.CharField(max_length=255, choices=const.LOAN_TYPE_CHOICES, default=const.LOAN_TYPE_12_MONTHS)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='loans')
+    returned_amount = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+
+    @property
+    def due_date(self):
+        return self.created_at + timedelta(int(self.type) / 12 * 365)
